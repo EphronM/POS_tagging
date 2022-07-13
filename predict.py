2af11ea458702a043990a0f2a4213942f287e7a1
Ephron M <ephronmartin2016@gmail.com>
2022-07-13 17:36:41 +0530

    prediction fun added

--- predict.py
+++ predict.py
@@ -1,11 +1,19 @@
 from keras.models import load_model
 from keras.preprocessing.sequence import pad_sequences
+import pickle
+
+with open('artifacts/tag_tokenizer.pkl', 'rb') as fp:
+    tag_tokenizer = pickle.load(fp)
+
+with open('artifacts/word_tokenizer.pkl', 'rb') as fp:
+    word_tokenizer = pickle.load(fp)
 
 
 model = load_model('gru_trained.h5')
 
 def try_me(trail):
-
+    MAX_SEQ_LENGTH = 100
+    
     x_trail = word_tokenizer.texts_to_sequences([trail])
     x_trail_paded = pad_sequences(
         x_trail, maxlen=MAX_SEQ_LENGTH, padding='pre', truncating='post')
